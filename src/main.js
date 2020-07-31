e81e20b0d426dfa75fa162c571ffaa7b02716030
Eric Berglund <ericberglund117@gmail.com>
2020-07-31 11:40:40 -0600

    Comment out functions to create test.js

---
+++ src/main.js
@@ -0,0 +1,75 @@
+//Media queries and global variables
+// var cells = document.querySelectorAll()
+
+// var wins = [];
+// // var player1 = new Player()
+// // var player2 = new Player()
+// var winningCombos = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
+// var gameActive = true;
+// var cellState = ['','','','','','','','',''];
+// var winningMessage = `${player.id} has won!`;
+// var drawMessage = 'Game ended in a draw!';
+// statusDisplay.innerHTML = currentPlayerTurn();
+// //eventListeners
+//
+// //functions
+//
+// function printBoard(playerToken) {
+//   var originalBoard =`
+//   <div class="cell">
+//     <img src="${playerToken}" alt="chosen-spot"
+//   </div>;`
+//
+// }
+//
+// function clickHandler(){
+//   var cell = originalBoard[i];
+//   var clickedCell = parseInt(cell.getAttribute(''))
+//   var currentMark = player1 ? circleClass : xClass;
+//   placeMark(cell, currentMark)
+// }
+
+
+// function getWinsFromLocalStorage() {
+//   if(localStorage.key('allWins')) {
+//     wins = [];
+//     var retrieveAllWins = localStorage.getItem('allWins');
+//     var parseAllWins = JSON.parse(retrieveAllWins) || [];
+//     for (var i = 0; i < parseAllWins.length; i++) {
+//       currentWins = new Player(parseAllWins[i].wins)
+//       wins.push(currentWins)
+//     }
+//   }
+// }
+
+
+// function handleCellPlayed() {
+//
+// }
+//
+// function handleResult(){
+//
+// }
+//
+// function handleRestartGame() {
+//
+// }
+//
+// function placeMark(cell, currentMak) {
+//
+// }
+// function markGameBoard(position, mark) {
+//   originalBoard.position = mark;
+// }
+//
+// function printBoard(originalBoard, playerToken) {
+//   var gameBoard =
