85a8f311c88a493e6fd6a88b759a137168d769e8
Eric Berglund <ericberglund117@gmail.com>
2020-08-04 17:09:43 -0600

    Fix checkwin function

--- src/game.js
+++ src/game.js
@@ -51,73 +51,78 @@ class Game {
       this.player1.moves.push(parseInt(event.target.getAttribute("data-num")))
       console.log(this.player1.moves)
       event.target.innerHTML = "X";
-      event.target.setAttribute("class","X");
+      event.target.style.fontSize = "2rem";
       displayTurn.innerHTML = "It is O's turn";
       this.counter++;
       this.player1Turn = false;
-      this.includesWin(this.winningCombos, this.player1.moves)
+      // this.includesWin(this.winningCombos, this.player1.moves)
       this.checkForWin(this.player1.moves, "X");
     } else {
       this.player2.moves.push(parseInt(event.target.getAttribute("data-num")))
       console.log(this.player2.moves)
       event.target.innerHTML = "O";
-      event.target.setAttribute("class","O");
+        event.target.style.fontSize = "2rem";
       displayTurn.innerHTML = "It is X's turn";
       this.counter++;
       this.player1Turn = true;
-      this.includesWin(this.winningCombos, this.player2.moves)
+      // this.includesWin(this.winningCombos, this.player2.moves)
       this.checkForWin(this.player2.moves, "O");
     }
+    this.checkforDraw();
   }
 
 
-  includesWin(winningCombos, moves) {
-    for (var i = 0; i < winningCombos.length; i++) {
-      if(!moves.includes(winningCombos[i])) {
-       this.win = false;
-      }
-    }
-    this.win = true;
-  }
+  // includesWin(winningCombos, moves) {
+  //   for (var i = 0; i < winningCombos.length; i++) {
+  //     console.log(moves)
+  //     if(!moves.includes(winningCombos[i])) {
+  //      this.win = false;
+  //     }
+  //   }
+  //   this.win = true;
+  // }
 
   disableEnableCells() {
     if(this.win) {
       this.gameActive = false;
       gameBoard.removeEventListener('click', clickHandler);
-    } else {
-      this.gameActive = true;
-      gameBoard.addEventListener('click', clickHandler);
     }
+    this.resetBoard();
   }
 
   checkForWin(movesArray, player) {
     // loop over the first array of winning combinations
     for (var i = 0; i < this.winningCombos.length; i++) {
-      if(this.includesWin(this.winningCombos[i], movesArray)) {
-          this.win
-          this.winCounter++
-          this.wins.push(this.winCounter)
-          console.log(this.wins)
-          this.disableEnableCells()
-          alert(`Game over. ${player} wins! Do you want to play again?`, 2000)
-          var delayRestart = window.setTimeout(this.resetBoard, 4000)
-        }
-
       //reset the cellCounter each time!
-    //   this.winCounter = 0;
-    //   // loop over each individual array
-    //   for (var j = 0; j < winningCombos[i].length; j++) {
-    //     // if the number in winning combo array is === a number in moves array, add to cellCounter
-    //     if(movesArray.indexOf(winningCombos[i][j]) !== -1) {
-    //     this.winCounter++;
-    //     }
-    //     // if cellCounter === 3 that means all 3 moves are winning combos and game is over!
-    // if(this.winCounter === 3) {
-          // timeout look at mdn and videos
-
-      // window.setTimeout(function() { alert(`Game over. ${player.name} wins!`); }, 3000)
+      this.winCounter = 0;
+      // loop over each individual array
+      for (var j = 0; j < this.winningCombos[i].length; j++) {
+        // if the number in winning combo array is === a number in moves array, add to cellCounter
+        if(movesArray.indexOf(this.winningCombos[i][j]) !== -1) {
+        this.winCounter++;
+      };
+        // if cellCounter === 3 that means all 3 moves are winning combos and game is over!
+    if(this.winCounter === 3) {
+        this.win = true;
+        this.counter = 0;
+        this.wins++
+        console.log(this.wins)
+        this.disableEnableCells()
+        alert(`Game over. ${player} wins! Do you want to play again?`, 2000)
+        var delayRestart = window.setTimeout(this.resetBoard, 2000)
       }
     }
+  }
+};
+    // if(this.includesWin(this.winningCombos[i], movesArray)) {
+      //     this.win
+      //     this.winCounter++
+      //     this.wins.push(this.winCounter)
+      //     console.log(this.wins)
+      //     this.disableEnableCells()
+      //     alert(`Game over. ${player} wins! Do you want to play again?`, 2000)
+      //     var delayRestart = window.setTimeout(this.resetBoard, 4000)
+      //   }
 
 
 
@@ -139,21 +144,14 @@ class Game {
   resetBoard() {
     if(this.win = true) {
       for (var i = cellsArray.length - 1; i >= 0; i--) {
-        cellsArray[i].innerHTML = "class";
+        cellsArray[i].innerHTML = "";
+        cellsArray[i].style.fontSize = "2rem";
         cellsArray[i].setAttribute("class","cell");
       }
       this.gameActive = true;
       firstPlayer.moves = [];
       secondPlayer.moves = [];
       this.counter = 0;
-    } else {
-      this.win = false;
-    }
-    this.restartGame();
-  }
-
-  restartGame() {
-    if(!this.gameActive) {
       this.gameActive = true;
       gameBoard.addEventListener('click', clickHandler);
     }
