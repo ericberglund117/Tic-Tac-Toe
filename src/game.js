261ebc71cacc24ec631477596f596098511a9314
Eric Berglund <ericberglund117@gmail.com>
2020-08-03 18:50:36 -0600

    Update game.js file to include game functions

--- src/game.js
+++ src/game.js
@@ -1,23 +1,142 @@
 class Game {
-  constructor(player) {
-    this.turn = currentPlayer.id;
-    this.move = currentPlayer.move;
+  constructor(firstPlayer, secondPlayer) {
+    this.player1 = firstPlayer;
+    this.player2 = secondPlayer;
+    this.gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    this.player1Turn = true;
     this.markPlaced = [];
+    this.wins = [];
   }
 
-  markGameBoard(position, mark) {
-    board.position = mark;
-    playMark();
+  start() {
+    addPlayerListener();
+    addResetListener();
   }
+
+  addPlayerListener() {
+    for(var i = 0; i < cellsArray.length - 1; i++) {
+      cellsArray[i].addEventListener("click", playMark)
+    }
+  }
+
+  // checkCurrentPlayer() {
+  //   if(this.player1Turn){
+  //     var currentPlayer = this.player1
+  //   } else {
+  //     var currentPlayer = this.player2
+  //   }
+  //   return currentPlayer
+  // }
+
+  playMark(){
+    if(this.player1Turn) {
+      this.player1.moves.push(parseInt(event.target.getAttribute("data-num")))
+      event.target.innerHTML = "X";
+      event.target.setAttribute("class","X");
+      displayTurn.innerHTML = "It is O's turn";
+      counter++;
+      this.player1Turn = false;
+    } else {
+      this.player2.moves.push(parseInt(event.target.getAttribute("data-num")))
+      event.target.innerHTML = "O";
+      event.target.setAttribute("class","O");
+      displayTurn.innerHTML = "It is X's turn";
+      counter++;
+      this.player1Turn = true;
+    }
+    this.checkForWin();
+    this.checkforDraw();
+      }
+
+
+  // includesWin(winningCombos, moves) {
+  //   for (var i = 0; i < winningCombos.length; i++) {
+  //     if(!moves.includes(winningCombos[i])) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+
+  checkForWin(movesArray, player) {
+    var winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ]
+    var movesArray = player.moves
+    // loop over the first array of winning combinations
+    for (var i = 0; i < winningCombos.length; i++) {
+      // reset the cellCounter each time!
+      cellCounter = 0;
+      // loop over each individual array
+      for (var j = 0; j < winningCombos[i].length; j++) {
+        // if the number in winning combo array is === a number in moves array, add to cellCounter
+        if(movesArray.indexOf(this.winningCombos[i][j]) !== -1) {
+        cellCounter++;
+        }
+        // if cellCounter === 3 that means all 3 moves are winning combos and game is over!
+    if(cellCounter === 3) {
+          // timeout look at mdn and videos
+      window.setTimeout(function() { alert(`Game over. ${player.name} wins!`); }, 3000)
+      this.resetBoard();
+        }
+      }
+    }
+  }
+
+    checkforDraw() {
+      if (counter >= 10) {
+        displayTurn.innerHTML = "Game Over!";
+        var conf = confirm("It's a draw, do you want to play again?");
+        if(conf){
+          resetBoard();
+        }
+      }
+    };
+
+
+  playerWinCounter(playerWins) {
+
+  };
+
+  addResetListener() {
+    var resetButton = document.querySelector("#reset");
+    resetButton.addEventListener("click", resetBoard);
+  }
+
+  resetBoard() {
+    for (var i = cellsArray.length - 1; i >= 0; i--) {
+      cellsArray[i].innerHTML = "cell";
+      cellsArray[i].setAttribute("class","cell");
+    }
+    oMoves = [];
+    xMoves = [];
+    cellCounter = 0;
+    counter = 1;
+    displayTurn.innerHTML = "It is X's turn";
+  }
+
 
   printBoard() {
-    createBoard();
+
 
   }
   swapTurns(Player){
     player1 = !player1
   }
 };
+
+
+
+
+  // if the counter is greater than or equal to 10, the game is a draw!
+
 
 
 
