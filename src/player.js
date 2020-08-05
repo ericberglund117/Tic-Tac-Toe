001e988cb968957ee8660d1501ec375a6c183d00
Eric Berglund <ericberglund117@gmail.com>
2020-08-04 21:24:22 -0600

    Update player.js to include localstorage functionality

--- src/player.js
+++ src/player.js
@@ -2,17 +2,29 @@ class Player {
   constructor(name, playerNumber) {
     this.name = name;
     this.playerNumber = playerNumber;
-    this.wins = 0;
+    this.wins = this.getWinsFromLocalStorage(name);
     this.moves = [];
   }
   saveWinsToStorage(player, wins) {
     var stringifyObject = JSON.stringify(wins);
     localStorage.setItem(player, stringifyObject);
   }
-  retrieveWinsFromStorage(array) {
-    this.getWinsFromLocalStorage(array)
+  retrieveWinsFromStorage(player) {
+    this.getWinsFromLocalStorage(player);
   }
-}
+
+  getWinsFromLocalStorage(player) {
+    if(localStorage.key(player)) {
+      var retrieveAllWins = localStorage.getItem(player);
+      var parseAllWins = JSON.parse(retrieveAllWins) || [];
+      // for (var i = 0; i < parseAllWins.length; i++) {
+        // player.wins = parseAllWins;
+        return parseAllWins;
+    } else {
+      return 0;
+    }
+  }
+};
 
 var firstPlayer = new Player('X', "firstPlayer");
 var secondPlayer = new Player("O", "secondPlayer");
