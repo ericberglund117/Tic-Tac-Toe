cdd77d2456fdf0fd1e23d143ad7b9c6c75f39b5b
Eric Berglund <ericberglund117@gmail.com>
2020-08-04 21:33:59 -0600

    Clean up comments and styling

--- src/game.js
+++ src/game.js
@@ -3,9 +3,7 @@ class Game {
     this.player1 = firstPlayer;
     this.player2 = secondPlayer;
     this.player1Turn = true;
-    // this.player1.wins = this.getWinsFromLocalStorage(this.player1)
-    // this.player2.wins = this.getWinsFromLocalStorage(this.player2)
-    this.wins = [firstPlayer.wins] || [secondPlayer.wins]
+    this.wins = [firstPlayer.wins] || [secondPlayer.wins];
     this.winCounter = 0;
     this.counter = 1;
     this.win = false;
@@ -19,12 +17,12 @@ class Game {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-      ]
-  }
+      ];
+  };
 
   start() {
     var game = new Game(firstPlayer, secondPlayer);
-  }
+  };
 
   checkCurrentPlayer() {
     if(this.player1Turn) {
@@ -32,13 +30,13 @@ class Game {
     } else {
       var currentPlayer = this.player2
     }
-    return currentPlayer
-  }
+    return currentPlayer;
+  };
 
-  playMark(){
+  playMark() {
     if(this.player1Turn) {
-      this.player1.moves.push(parseInt(event.target.getAttribute("data-num")))
-      console.log(this.player1.moves)
+      this.player1.moves.push(parseInt(event.target.getAttribute("data-num")));
+      console.log(this.player1.moves);
       event.target.innerHTML = "X";
       event.target.style.fontSize = "2rem";
       displayTurn.innerHTML = "It is O's turn";
@@ -46,8 +44,8 @@ class Game {
       this.player1Turn = false;
       this.checkForWin(this.player1.moves, "X");
     } else {
-      this.player2.moves.push(parseInt(event.target.getAttribute("data-num")))
-      console.log(this.player2.moves)
+      this.player2.moves.push(parseInt(event.target.getAttribute("data-num")));
+      console.log(this.player2.moves);
       event.target.innerHTML = "O";
       event.target.style.fontSize = "2rem";
       displayTurn.innerHTML = "It is X's turn";
@@ -56,7 +54,7 @@ class Game {
       this.checkForWin(this.player2.moves, "O");
     }
     this.checkforDraw();
-  }
+  };
 
   disableEnableCells() {
     if(this.win) {
@@ -64,33 +62,33 @@ class Game {
       gameBoard.removeEventListener('click', clickHandler);
     }
     this.resetBoard();
-  }
+  };
 
   checkForWin(movesArray, player) {
     for (var i = 0; i < this.winningCombos.length; i++) {
       this.winCounter = 0;
       for (var j = 0; j < this.winningCombos[i].length; j++) {
-        if(movesArray.indexOf(this.winningCombos[i][j]) !== -1) {
-        this.winCounter++;
-      };
-    if(this.winCounter === 3) {
-        this.win = true;
-        this.counter = 0;
-        if(player === "X") {
-          this.player1.wins++;
-          this.player1.saveWinsToStorage(player, this.player1.wins);
-        } else {
-          this.player2.wins++;
-          this.player2.saveWinsToStorage(player, this.player2.wins);
+          if(movesArray.indexOf(this.winningCombos[i][j]) !== -1) {
+          this.winCounter++;
         };
-        alert(`Game over. ${player} wins! Do you want to play again?`, 2000)
-        this.disableEnableCells();
-        var delayRestart = window.setTimeout(this.resetBoard, 2000)
-        this.countWins();
+      if(this.winCounter === 3) {
+          this.win = true;
+          this.counter = 0;
+          if(player === "X") {
+            this.player1.wins++;
+            this.player1.saveWinsToStorage(player, this.player1.wins);
+          } else {
+            this.player2.wins++;
+            this.player2.saveWinsToStorage(player, this.player2.wins);
+          };
+          alert(`Game over. ${player} wins! Do you want to play again?`, 2000)
+          this.disableEnableCells();
+          var delayRestart = window.setTimeout(this.resetBoard, 2000)
+          this.countWins();
+        };
       };
     };
   };
-};
 
   countWins() {
     if(this.player1.wins > 0) {
@@ -98,18 +96,18 @@ class Game {
     }
     if(this.player2.wins > 0) {
       player2WinCounter.innerHTML = `${this.player2.wins} Wins`;
-    }
-  }
-
-    checkforDraw() {
-      if (this.counter >= 10) {
-        displayTurn.innerHTML = "Game Over!";
-        var conf = confirm("It's a draw, do you want to play again?");
-        if(conf){
-          this.resetBoard();
-        }
-      }
     };
+  };
+
+  checkforDraw() {
+    if (this.counter >= 10) {
+      displayTurn.innerHTML = "Game Over!";
+      var conf = confirm("It's a draw, do you want to play again?");
+      if(conf){
+        this.resetBoard();
+      };
+    };
+  };
 
   resetBoard() {
     if(this.win = true) {
@@ -117,25 +115,13 @@ class Game {
         cellsArray[i].innerHTML = "";
         cellsArray[i].style.fontSize = "2rem";
         cellsArray[i].setAttribute("class","cell");
-      }
+      };
       this.gameActive = true;
       firstPlayer.moves = [];
       secondPlayer.moves = [];
       this.counter = 0;
       this.gameActive = true;
       gameBoard.addEventListener('click', clickHandler);
-    }
-  }
-
-  // getWinsFromLocalStorage(player) {
-  //   if(localStorage.key(player.name)) {
-  //     var retrieveAllWins = localStorage.getItem(player.name);
-  //     var parseAllWins = JSON.parse(retrieveAllWins) || [];
-  //     // for (var i = 0; i < parseAllWins.length; i++) {
-  //       // player.wins = parseAllWins;
-  //       return parseAllWins
-  //   }
-  // };
-
-
+    };
+  };
 };
--- src/player.js
+++ src/player.js
@@ -4,21 +4,21 @@ class Player {
     this.playerNumber = playerNumber;
     this.wins = this.getWinsFromLocalStorage(name);
     this.moves = [];
-  }
+  };
+
   saveWinsToStorage(player, wins) {
     var stringifyObject = JSON.stringify(wins);
     localStorage.setItem(player, stringifyObject);
-  }
+  };
+
   retrieveWinsFromStorage(player) {
     this.getWinsFromLocalStorage(player);
-  }
+  };
 
   getWinsFromLocalStorage(player) {
     if(localStorage.key(player)) {
       var retrieveAllWins = localStorage.getItem(player);
       var parseAllWins = JSON.parse(retrieveAllWins) || [];
-      // for (var i = 0; i < parseAllWins.length; i++) {
-        // player.wins = parseAllWins;
         return parseAllWins;
     } else {
       return 0;
@@ -28,7 +28,3 @@ class Player {
 
 var firstPlayer = new Player('X', "firstPlayer");
 var secondPlayer = new Player("O", "secondPlayer");
-
-
-
-//need to find a way to diff between player 1 and player 2 wins in order to save into local storage
