5acbc87b2f251d319b905b2c84eafe270356977a
Eric Berglund <ericberglund117@gmail.com>
2020-08-04 19:42:59 -0600

    Begin localStorage

--- src/player.js
+++ src/player.js
@@ -5,16 +5,18 @@ class Player {
     this.wins = 0;
     this.moves = [];
   }
-  saveWinsToStorage(array) {
-    var stringifyObject = JSON.stringify(array);
-    localStorage.setItem('allWins', stringifyObject);
+  saveWinsToStorage(player, wins) {
+    var stringifyObject = JSON.stringify(wins);
+    localStorage.setItem(player, stringifyObject);
   }
   retrieveWinsFromStorage(array) {
     this.getWinsFromLocalStorage(array)
   }
 }
 
-var firstPlayer = new Player('X', "firstPlayer")
-var secondPlayer = new Player("O", "secondPlayer")
+var firstPlayer = new Player('X', "firstPlayer");
+var secondPlayer = new Player("O", "secondPlayer");
+
+
 
 //need to find a way to diff between player 1 and player 2 wins in order to save into local storage
